0433c4984daacf376510e629329cfbe2a9c85e28
Gideon Billings <gidobot@umich.edu>
2023-12-14 16:59:51 +1100

    Updates for testing. In air dataset tests fine, though lens printed aperature values seem innaccurate after 1.4. Need to figure out channel exposure discrepency for tank datasets

--- src/wateratenuationmodel.py
+++ src/wateratenuationmodel.py
@@ -14,6 +14,7 @@ class WaterPropagation:
         self.attenuation_coef = []
         self.initialized = False
 
+        # self.jerlov_wavelenths = [300, 310, 350, 375, 400, 425, 450, 475, 500, 525, 550, 575, 600, 625, 650, 675, 700, 750, 800]
         self.jerlov_wavelenths = [300, 310, 350, 375, 400, 425, 450, 475, 500, 525, 550, 575, 600, 625, 650, 675, 700]
 
     def reset(self):
@@ -44,6 +45,36 @@ class WaterPropagation:
         distance_map = np.tile(distance_map, (1,1,len(wavelengths)))
         return np.exp(-b*distance_map)
 
+    def load_air_profile(self):
+        # Kd - downard diffuse attenuation coefficient depends on sun azimuth angle and other factors not
+        # inherent to the physical water properties. However, it also captures the forward scattering
+        # component, whereas the backscatter attenuation coefficient by itself does not capture this. Therefore,
+        # it is inaccurate to assume all scattered light is lost, and Kd provides a compromise in estimating
+        # the true attenuation of the light signal. All light sourced from the irradiated scene that is scattered
+        # contributes to haze, even though it is not "attenuated". A future feature may provide an estimate of
+        # the haze signal. 
+        Kd = np.zeros(len(self.jerlov_wavelenths))
+        self.attenuation_coef = Kd # np.array(a) + np.array(b)
+        self.initialized = True
+
+    def load_tank_profile(self):
+        # Kd - downard diffuse attenuation coefficient depends on sun azimuth angle and other factors not
+        # inherent to the physical water properties. However, it also captures the forward scattering
+        # component, whereas the backscatter attenuation coefficient by itself does not capture this. Therefore,
+        # it is inaccurate to assume all scattered light is lost, and Kd provides a compromise in estimating
+        # the true attenuation of the light signal. All light sourced from the irradiated scene that is scattered
+        # contributes to haze, even though it is not "attenuated". A future feature may provide an estimate of
+        # the haze signal. 
+        # Kd = [0.17, 0.15, 0.06, 0.01, 0.007, 0.005, 0.01, 0.014, 0.023, 0.048, 0.066, 0.084, 0.15, 0.15, 0.2, 0.28, 0.28, 0.25, 0.23]
+        Kd = [0.17, 0.15, 0.06, 0.01, 0.007, 0.005, 0.01, 0.014, 0.023, 0.048, 0.066, 0.084, 0.15, 0.15, 0.2, 0.28, 0.28]
+        self.attenuation_coef = Kd # np.array(a) + np.array(b)
+        self.initialized = True
+        import matplotlib.pyplot as plt
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.plot(self.jerlov_wavelenths, Kd)
+        plt.show()
+
     def load_pure_profile(self):
         # Kd - downard diffuse attenuation coefficient depends on sun azimuth angle and other factors not
         # inherent to the physical water properties. However, it also captures the forward scattering
@@ -55,6 +86,11 @@ class WaterPropagation:
         Kd = [0.17, 0.15, 0.06, 0.01, 0.007, 0.005, 0.01, 0.014, 0.023, 0.048, 0.066, 0.084, 0.250, 0.313, 0.373, 0.452, 0.604]
         self.attenuation_coef = Kd # np.array(a) + np.array(b)
         self.initialized = True
+        import matplotlib.pyplot as plt
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.plot(self.jerlov_wavelenths, Kd)
+        plt.show()
 
     def load_jerlovI_profile(self):
         # Kd - downard diffuse attenuation coefficient depends on sun azimuth angle and other factors not
